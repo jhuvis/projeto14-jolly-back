401002838658579a923373d57d3f4fb9fa271cdb
Leonardo de Souza Ferreira <leonardosouzaf@icloud.com>
2022-09-14 17:21:04 -0300

    initial correction

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/index.js'; 
 import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const router = express.Router();
+router.use(authRoutes);
+
+
+
+
+
 app.use(router);
 
-app.listen(process.env.port);
+app.listen(process.env.PORT);
--- routes/index.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import authRoutes from './authRoutes.js';
-
-const router = express.Router();
-router.use(authRoutes);
-export default router;
